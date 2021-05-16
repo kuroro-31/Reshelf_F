4444c8307b4a4f5a229c662d07df48da77647576
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2021-05-16 22:18:49 +0900

    検索画面サイドバー追加

---
+++ components/layout/sidebar/SidebarSearch.vue
@@ -0,0 +1,165 @@
+<template>
+  <div>
+    <div v-for="(category, index) in categories" :key="index">
+      <!-- 親カテゴリー -->
+      <div
+        :class="{ 'toggle-on': category.toggleOn }"
+        class="
+          cate-ttl
+          w-full
+          relative
+          inline-flex
+          justify-between
+          items-center
+        "
+        @click="category.toggleOn = !category.toggleOn"
+      >
+        <a
+          href="#"
+          class="title font-bold text-xl hover:text-blue flex-shrink-0"
+        >
+          {{ category.name }}
+        </a>
+        <div
+          v-if="category.subcategories.length != 0"
+          class="w-full relative flex justify-end"
+        >
+          <chevron-right-icon
+            size="1x"
+            class="arrow flex-shrink-0"
+          ></chevron-right-icon>
+        </div>
+      </div>
+
+      <!-- 子カテゴリー -->
+      <ul
+        v-for="subcategory in category.subcategories"
+        :key="subcategory"
+        :class="{ show: category.toggleOn }"
+        class="cate-items"
+      >
+        <li>
+          <vs-checkbox v-model="checkBox" class="checkbox">
+            {{ subcategory }}
+          </vs-checkbox>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+//Icons
+import { ChevronRightIcon } from 'vue-feather-icons'
+
+export default {
+  components: {
+    ChevronRightIcon,
+  },
+  data() {
+    return {
+      checkBox: '',
+      categories: [
+        {
+          name: '評価',
+          subcategories: ['4.5以上', '4.0以上', '3.5以上'],
+          toggleOn: true,
+        },
+        {
+          name: '値段',
+          subcategories: ['2,500円', '5,000円', '7,500円', '10,000円'],
+          toggleOn: true,
+        },
+        {
+          name: 'トピック',
+          subcategories: [
+            'Javascript',
+            'React',
+            'Vue.js',
+            'Laravel',
+            'Ruby',
+            'AWS',
+            'Python',
+            'HTML',
+            'CSS',
+          ],
+          toggleOn: true,
+        },
+        {
+          name: 'レベル',
+          subcategories: ['初級', '中級', '上級', '特級', 'すべて'],
+          toggleOn: true,
+        },
+        {
+          name: '言語',
+          subcategories: ['英語', '日本語'],
+          toggleOn: true,
+        },
+        {
+          name: '特徴',
+          subcategories: ['デモページ有り', 'サブスク対象教材'],
+          toggleOn: true,
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.cate-ttl {
+  padding: 8px;
+  margin-bottom: 3.5px;
+  transition: all 0.2s ease;
+  > a {
+    transition: inherit;
+    max-width: calc(100% - 14px);
+  }
+  .arrow {
+    transition: all 0.2s ease-out;
+    transform: translateX(0) rotate(90deg);
+  }
+
+  &:hover {
+    @apply cursor-pointer;
+    transform: translateX(5px);
+    background-color: rgba($primary, 0.08);
+    border-radius: 6px;
+  }
+  &.toggle-on {
+    border-radius: 6px;
+    .arrow {
+      transform: rotate(270deg);
+    }
+  }
+}
+.cate-items {
+  max-height: 0;
+  opacity: 0;
+  transition: all 0.2s ease;
+  &.show {
+    opacity: 1;
+    max-height: 100vh;
+  }
+  > li {
+    padding: 10px;
+    transition: all 0.2s ease;
+    &:hover {
+      @apply cursor-pointer;
+      transform: translateX(5px);
+      background-color: rgba($primary, 0.08);
+      border-radius: 6px;
+    }
+    .feather.feather-circle {
+      margin-top: 8px;
+    }
+  }
+}
+.title {
+  color: var(--color);
+}
+.checkbox {
+  @apply w-full flex;
+  justify-content: flex-start;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,274 +1,30 @@
 <template>
   <div class="w-full h-full mx-auto flex flex-col">
     <HeaderNav />
-    <main class="w-full flex mx-auto">
-      <all-item :items="items" />
+    <main class="w-full flex max-w-screen-xl mx-auto container">
+      <div class="lg:flex w-full">
+        <nav class="docs-index lg:max-h-(screen-22) pin-22">
+          <SidebarSearch />
+        </nav>
+        <div class="markdown-body min-h-(screen-16)"></div>
+      </div>
     </main>
+    <!-- <FooterNav /> -->
   </div>
 </template>
 <script>
+// layout
 import HeaderNav from '@/components/layout/HeaderNav'
-import AllItem from '@/components/atoms/item/AllItem'
+// import FooterNav from '@/components/layout/FooterNav'
+import SidebarSearch from '@/components/layout/sidebar/SidebarSearch'
 export default {
   components: {
     HeaderNav,
-    AllItem,
+    // FooterNav,
+    SidebarSearch,
   },
   data() {
-    return {
-      items: [
-        {
-          // キー
-          id: '1',
-          // 画像
-          src: 'https://i.gyazo.com/92f8c7d85e6e0e159996a15fb00a9ef4.png',
-          alt: 'components item',
-          // タイトル
-          title: 'Foo',
-          // 作者
-          name: 'クラピカ',
-          // タグ
-          tags: ['phase1', 'btn', 'rakuten'],
-          // 日付
-          add_time: '2021/06/23',
-          edit_time: '2021/06/23',
-          // URL
-          mock_urls: [
-            'http://localhost:8888',
-            'http://localhost:8888',
-            'http://localhost:8888',
-          ],
-          front_urls: [
-            'http://localhost:8080/jp',
-            'http://localhost:8080/jp',
-            'http://localhost:8080/jp',
-          ],
-          dev_urls: [
-            'http://localhost:8888',
-            'http://localhost:8888',
-            'http://localhost:8888',
-          ],
-          real_urls: [
-            'http://localhost:8888',
-            'http://localhost:8888',
-            'http://localhost:8888',
-          ],
-          // ファイル名
-          mock_passs: [
-            'mock/components/atoms/item/AllItem.vue',
-            'mock/components/atoms/item/AllItem.vue',
-            'mock/components/atoms/item/AllItem.vue',
-            'mock/components/atoms/item/AllItem.vue',
-          ],
-          front_passs: [
-            'front/components/atoms/item/AllItem.vue',
-            'front/components/atoms/item/AllItem.vue',
-            'front/components/atoms/item/AllItem.vue',
-            'front/components/atoms/item/AllItem.vue',
-          ],
-        },
-        {
-          // キー
-          id: '2',
-          // 画像
-          src: 'https://i.gyazo.com/4b894883753296442386f8e24952f104.jpg',
-          alt: 'components item',
-          // タイトル
-          title: 'Foo',
-          // 作者
-          name: 'クロロ・ルシルフル',
-          // タグ
-          tags: ['phase1', 'btn', 'rakuten'],
-          // 日付
-          add_time: '2021/06/23',
-          edit_time: '2021/06/23',
-          // URL
-          mock_urls: [
-            'http://localhost:8888',
-            'http://localhost:8888',
-            'http://localhost:8888',
-          ],
-          front_urls: [
-            'http://localhost:8080/jp',
-            'http://localhost:8080/jp',
-            'http://localhost:8080/jp',
-          ],
-          dev_urls: [
-            'http://localhost:8888',
-            'http://localhost:8888',
-            'http://localhost:8888',
-          ],
-          real_urls: [
-            'http://localhost:8888',
-            'http://localhost:8888',
-            'http://localhost:8888',
-          ],
-          // ファイル名
-          mock_passs: [
-            'mock/components/atoms/item/AllItem.vue',
-            'mock/components/atoms/item/AllItem.vue',
-            'mock/components/atoms/item/AllItem.vue',
-            'mock/components/atoms/item/AllItem.vue',
-          ],
-          front_passs: [
-            'front/components/atoms/item/AllItem.vue',
-            'front/components/atoms/item/AllItem.vue',
-            'front/components/atoms/item/AllItem.vue',
-            'front/components/atoms/item/AllItem.vue',
-          ],
-        },
-        {
-          // キー
-          id: '3',
-          // 画像
-          src: 'https://i.gyazo.com/682701b49b267640ea8300a3e3d7bba9.png',
-          alt: 'components item',
-          // タイトル
-          title: 'Foo',
-          // 作者
-          name: 'フェイタン',
-          // タグ
-          tags: ['phase1', 'btn', 'rakuten'],
-          // 日付
-          add_time: '2021/06/23',
-          edit_time: '2021/06/23',
-          // URL
-          mock_urls: [
-            'http://localhost:8888',
-            'http://localhost:8888',
-            'http://localhost:8888',
-          ],
-          front_urls: [
-            'http://localhost:8080/jp',
-            'http://localhost:8080/jp',
-            'http://localhost:8080/jp',
-          ],
-          dev_urls: [
-            'http://localhost:8888',
-            'http://localhost:8888',
-            'http://localhost:8888',
-          ],
-          real_urls: [
-            'http://localhost:8888',
-            'http://localhost:8888',
-            'http://localhost:8888',
-          ],
-          // ファイル名
-          mock_passs: [
-            'mock/components/atoms/item/AllItem.vue',
-            'mock/components/atoms/item/AllItem.vue',
-            'mock/components/atoms/item/AllItem.vue',
-            'mock/components/atoms/item/AllItem.vue',
-          ],
-          front_passs: [
-            'front/components/atoms/item/AllItem.vue',
-            'front/components/atoms/item/AllItem.vue',
-            'front/components/atoms/item/AllItem.vue',
-            'front/components/atoms/item/AllItem.vue',
-          ],
-        },
-        {
-          // キー
-          id: '4',
-          // 画像
-          src: 'https://i.gyazo.com/27bad6b0196fe465f8e74eb1067709ed.png',
-          alt: 'components item',
-          // タイトル
-          title: 'Foo',
-          // 作者
-          name: 'ヒソカ',
-          // タグ
-          tags: ['phase1', 'btn', 'rakuten'],
-          // 日付
-          add_time: '2021/06/23',
-          edit_time: '2021/06/23',
-          // URL
-          mock_urls: [
-            'http://localhost:8888',
-            'http://localhost:8888',
-            'http://localhost:8888',
-          ],
-          front_urls: [
-            'http://localhost:8080/jp',
-            'http://localhost:8080/jp',
-            'http://localhost:8080/jp',
-          ],
-          dev_urls: [
-            'http://localhost:8888',
-            'http://localhost:8888',
-            'http://localhost:8888',
-          ],
-          real_urls: [
-            'http://localhost:8888',
-            'http://localhost:8888',
-            'http://localhost:8888',
-          ],
-          // ファイル名
-          mock_passs: [
-            'mock/components/atoms/item/AllItem.vue',
-            'mock/components/atoms/item/AllItem.vue',
-            'mock/components/atoms/item/AllItem.vue',
-            'mock/components/atoms/item/AllItem.vue',
-          ],
-          front_passs: [
-            'front/components/atoms/item/AllItem.vue',
-            'front/components/atoms/item/AllItem.vue',
-            'front/components/atoms/item/AllItem.vue',
-            'front/components/atoms/item/AllItem.vue',
-          ],
-        },
-        {
-          // キー
-          id: '5',
-          // 画像
-          src: 'https://i.gyazo.com/eba895df6ab107f310e53b4f68d18bfd.png',
-          alt: 'components item',
-          // タイトル
-          title: 'Foo',
-          // 作者
-          name: 'マチ',
-          // タグ
-          tags: ['phase1', 'btn', 'rakuten'],
-          // 日付
-          add_time: '2021/06/23',
-          edit_time: '2021/06/23',
-          // URL
-          mock_urls: [
-            'http://localhost:8888',
-            'http://localhost:8888',
-            'http://localhost:8888',
-          ],
-          front_urls: [
-            'http://localhost:8080/jp',
-            'http://localhost:8080/jp',
-            'http://localhost:8080/jp',
-          ],
-          dev_urls: [
-            'http://localhost:8888',
-            'http://localhost:8888',
-            'http://localhost:8888',
-          ],
-          real_urls: [
-            'http://localhost:8888',
-            'http://localhost:8888',
-            'http://localhost:8888',
-          ],
-          // ファイル名
-          mock_passs: [
-            'mock/components/atoms/item/AllItem.vue',
-            'mock/components/atoms/item/AllItem.vue',
-            'mock/components/atoms/item/AllItem.vue',
-            'mock/components/atoms/item/AllItem.vue',
-          ],
-          front_passs: [
-            'front/components/atoms/item/AllItem.vue',
-            'front/components/atoms/item/AllItem.vue',
-            'front/components/atoms/item/AllItem.vue',
-            'front/components/atoms/item/AllItem.vue',
-          ],
-        },
-      ],
-    }
+    return {}
   },
 }
 </script>
@@ -276,5 +32,11 @@ export default {
 main {
   height: calc(100vh - 61px);
   overflow-y: hidden;
+}
+.docs-index {
+  @apply hidden w-full lg:block lg:w-1/4 xl:w-1/5 p-6 lg:border-r z-10 lg:sticky overflow-y-auto;
+}
+.markdown-body {
+  @apply w-full lg:w-3/4 xl:w-4/5 pt-10 px-6 lg:px-12;
 }
 </style>
