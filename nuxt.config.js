64fbf4fca11f99653bda39b592a151161f5f4d8a
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2021-05-16 16:53:07 +0900

    整理

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,34 +4,53 @@ export default {
   ssr: true,
 
   head: {
-    title: 'Reshelf【リシェルフ】| アップデートするオンライン学習マーケットプレイス',
+    title:
+      'Reshelf【リシェルフ】| アップデートするオンライン学習マーケットプレイス',
     htmlAttrs: {
       lang: 'ja',
-      prefix: 'og: http://ogp.me/ns#'
+      prefix: 'og: http://ogp.me/ns#',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description',
-        content: 'Reshelfは、アップデートするオンライン学習マーケットプレイスです。学びたい受講者と、教えたい講師をオンラインでつなぐお手伝いをします。'
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Reshelfは、アップデートするオンライン学習マーケットプレイスです。学びたい受講者と、教えたい講師をオンラインでつなぐお手伝いをします。',
       },
       { hid: 'og:site_name', property: 'og:site_name', content: 'Reshelf' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       { hid: 'og:url', property: 'og:url', content: 'http://www.reshelf.jp/' },
-      { hid: 'og:title', property: 'og:title', content: 'Reshelf【リシェルフ】| アップデートするオンライン学習マーケットプレイス' },
-      { hid: 'og:description', property: 'og:description', content: 'Reshelfは、アップデートするオンライン学習マーケットプレイスです。学びたい受講者と、教えたい講師をオンラインでつなぐお手伝いをします。' },
-      { hid: 'og:image', property: 'og:image', content: 'https://res.cloudinary.com/reshelf/image/upload/v1619870944/ogp_rzf3q1.png' },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content:
+          'Reshelf【リシェルフ】| アップデートするオンライン学習マーケットプレイス',
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          'Reshelfは、アップデートするオンライン学習マーケットプレイスです。学びたい受講者と、教えたい講師をオンラインでつなぐお手伝いをします。',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content:
+          'https://res.cloudinary.com/reshelf/image/upload/v1619870944/ogp_rzf3q1.png',
+      },
       { name: 'twitter:card', content: 'summary_large_image' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   css: ['ress', { src: '@/assets/sass/app.scss', lang: 'scss' }],
 
   // パッケージ関係
-  plugins: ['~/plugins/vuesax.js'],
+  plugins: [
+    // '~/plugins/vuesax.js',
+  ],
 
   components: true,
 
