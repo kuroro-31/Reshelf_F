0249219f52d731e431dac45828eaa018bedfdf3a
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2021-05-16 22:17:52 +0900

    vuesax導入

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,16 @@
     <Nuxt />
   </div>
 </template>
+<script>
+export default {
+  mounted() {
+    this.$vs.theme({
+      primary: '#33C4CC',
+      dark: '#626262',
+      success: '#28C76F',
+      warning: '#F89A1E',
+      danger: '#EA5555',
+    })
+  },
+}
+</script>
--- plugins/vuesax.js
+++ plugins/vuesax.js
@@ -1,7 +1,7 @@
+// Vuesax Component Framework
+import 'material-icons/iconfont/material-icons.css' //Material Icons
 import Vue from 'vue'
 import Vuesax from 'vuesax'
-import 'vuesax/dist/vuesax.css' // Vuesax styles
+import 'vuesax/dist/vuesax.css' // Vuesax
 
-Vue.use(Vuesax, {
-  // options here
-})
+Vue.use(Vuesax)
