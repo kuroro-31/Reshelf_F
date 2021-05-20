17887bc900b653fe02651bb14cb442503d3b8cef
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2021-05-20 20:29:43 +0900

    エラー画面修正

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,24 +2,32 @@
   <div class="w-full h-full mx-auto flex flex-col">
     <HeaderNav />
     <main class="w-full flex items-center justify-center mx-auto">
-      <h1 v-if="error && error.statusCode === 404" class="text-6xl font-bold">
-        {{ error.statusCode }}
-        <br />
-        ページが見つかりません
-      </h1>
-      <h1 v-else-if="error && error.message" class="text-6xl font-bold">
-        {{ error.message }}
-        <br />
-        エラーが発生しました
-      </h1>
+      <ul class="flex flex-col">
+        <li
+          v-for="(value, key, index) in error"
+          :key="index"
+          class="text-6xl font-bold flex message"
+        >
+          {{ key }}
+          {{ value }}
+        </li>
+        <nuxt-link to="/">
+          <re-button class="re-button w-32 mt-8">
+            <button class="re-button-primary-filled">HOME</button>
+          </re-button>
+        </nuxt-link>
+      </ul>
     </main>
   </div>
 </template>
 <script>
 import HeaderNav from '@/components/layout/HeaderNav'
+import ReButton from '@/components/atoms/button/ReButton'
+
 export default {
   components: {
-    HeaderNav
+    HeaderNav,
+    ReButton
   },
   props: {
     error: {
@@ -29,6 +37,18 @@ export default {
   },
   data() {
     return {}
+  },
+  head() {
+    return {
+      title: this.error.message,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.error.message
+        }
+      ]
+    }
   }
 }
 </script>
@@ -36,5 +56,8 @@ export default {
 main {
   height: calc(100vh - 61px);
   overflow-y: hidden;
+}
+.message {
+  color: var(--sub-color);
 }
 </style>
