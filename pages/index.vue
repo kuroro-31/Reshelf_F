9377f2342ccd26501f48e841d5d43e9cd69dc1eb
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2021-06-12 00:51:46 +0900

    トップ修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="w-full h-screen mx-auto flex flex-col scroll-none">
+  <div class="w-full mx-auto flex flex-col scroll-none">
     <HeaderNav />
     <main class="w-full flex max-w-screen-xl mx-auto container">
-      <div class="lg:flex w-full">
+      <div class="lg:flex w-full -mx-6">
         <nav class="side-nav lg:max-h-(screen-22) pin-22">
           <SidebarSearch />
         </nav>
@@ -13,13 +13,13 @@
         </div>
       </div>
     </main>
-    <!-- <FooterNav /> -->
+    <FooterNav />
   </div>
 </template>
 <script>
 // layout
 import HeaderNav from '@/components/layout/HeaderNav'
-// import FooterNav from '@/components/layout/FooterNav'
+import FooterNav from '@/components/layout/FooterNav'
 import SidebarSearch from '@/components/layout/sidebar/SidebarSearch'
 // atoms
 // import HeroItem from '@/components/atoms/item/HeroItem'
@@ -29,7 +29,7 @@ export default {
   auth: false,
   components: {
     HeaderNav,
-    // FooterNav,
+    FooterNav,
     SidebarSearch,
     // HeroItem,
     AllItem,
@@ -322,21 +322,21 @@ export default {
 <style lang="scss" scoped>
 .side-nav {
   @apply hidden w-full lg:block lg:w-1/4 xl:w-1/5 z-10 lg:sticky overflow-y-auto;
-  @screen lg {
-    border-right: 1px var(--thin-gray) solid;
-  }
-  @screen lg {
-    @apply overflow-y-auto;
-    height: calc(100vh - 68px);
-  }
+  // @screen lg {
+  //   border-right: 1px #ddd solid;
+  // }
+  // @screen lg {
+  //   @apply overflow-y-auto;
+  //   height: calc(100vh - 68px);
+  // }
 }
 .main-body {
   // @apply w-full lg:w-3/4 xl:w-3/5 pt-10 px-6 lg:px-12;
   // @apply w-full lg:w-3/4 xl:w-3/5 p-6;
-  @apply w-full lg:w-3/4 xl:w-4/5 pt-10 px-10 lg:pr-0;
-  @screen lg {
-    @apply overflow-y-auto;
-    height: calc(100vh - 68px);
-  }
+  @apply w-full lg:w-3/4 xl:w-4/5 pt-10 px-6 lg:px-12;
+  // @screen lg {
+  //   @apply overflow-y-auto;
+  //   height: calc(100vh - 68px);
+  // }
 }
 </style>
