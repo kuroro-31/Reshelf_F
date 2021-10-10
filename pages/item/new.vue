f8494ccd4f23459399e2fb6eefbb2e32cda87171
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2021-10-10 21:44:08 +0900

    修正

--- pages/item/new.vue
+++ pages/item/new.vue
@@ -149,11 +149,12 @@ export default {
         spellcheck: true,
         body_class: 'reshelf-editor',
         selector: 'textarea',
+        toc_header: 'div',
         plugins: [
           'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
         ],
         toolbar:
-          'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+          'toc undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
         insert_toolbar: 'quickimage quicktable emoticons',
         color_map: [
           '#BFEDD2',
