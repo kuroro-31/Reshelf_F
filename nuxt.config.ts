64e583b666e4426d1ace0035bb71b1f0fd4ef9a8
Kuroro <shun.ichiro.tanaka16@gmail.com>
2022-02-04 12:11:55 +0900

    tailwindとscssの設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,18 @@ import { defineNuxtConfig } from 'nuxt3'
 export default defineNuxtConfig({
   typescript: {
     strict: true
-  }
+  },
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+  css: [
+    "@/assets/scss/tailwind.scss"
+  ],
 })
