8221bb739a09866ae4b9d63ab6a121cd861ad995
TARO <shun.ichiro.tanaka16@gmail.com>
2022-02-07 03:59:28 +0900

    eslint 設定

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,5 @@
 <template>
-  <h1 class="m-4 text-2xl text-white bg-green-400">This is index Page</h1>
+  <h1 class="m-4 text-2xl text-white bg-green-400">
+    This is index Page
+  </h1>
 </template>
